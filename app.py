0dc21d7d287f7f52b492bb305af735e144ab0f03
Olajide Olufemi <Olufemi.Olajide@alderhey.nhs.uk>
2021-10-26 11:38:16 +0100

    app created and tested

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import os
 import numpy as np
 from prediction_service import prediction
-import yaml
-import joblib
 
-params_path = "params.yaml"
 
 webapp_root = "webapp"
 
@@ -19,8 +16,21 @@ app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
 def index():
 
     if request.method == "POST":
-        pass
-        
+        try:
+            if request.form:
+                dict_req = dict(request.form)
+                response = prediction.form_response(dict_req)
+                return render_template("index.html", response=response)
+            elif request.json:
+                response = prediction.api_response(request.json)
+                return jsonify(response)
+
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went wrong!! Try again later!"}
+            error = {"error": e}
+
+            return render_template("404.html", error=error)
     else:
         return render_template("index.html")
 
