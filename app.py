1ad92b46c943a742382a9c1fd8af3646ed7be078
Olajide Olufemi <Olufemi.Olajide@alderhey.nhs.uk>
2021-10-26 11:37:58 +0100

    app created and tested

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-ECHO is on.
+from flask import Flask, render_template, request, jsonify
+import os
+import numpy as np
+from prediction_service import prediction
+import yaml
+import joblib
+
+params_path = "params.yaml"
+
+webapp_root = "webapp"
+
+static_dir = os.path.join(webapp_root, "static")
+template_dir = os.path.join(webapp_root, "templates")
+
+app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    if request.method == "POST":
+        pass
+        
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
